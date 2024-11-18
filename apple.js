6dbffd976f4fcbbfcd50c62073c4183f7d1c201a
martinskyler00 <112213373+martinskyler00@users.noreply.github.com>
2024-11-18 16:37:37 -0600

    front page done

--- apple.js
+++ apple.js
@@ -1,9 +1,10 @@
-// Smooth scroll for navigation links
 document.querySelectorAll('.nav-links a').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
+    // Remove this if you don't want to override default behavior
     e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
 });
+
